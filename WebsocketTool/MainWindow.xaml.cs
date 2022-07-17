2a9bc46613a28b547a4fa89531803e8567ca19c9
atsus <atsushinee@outlook.com>
2022-07-17 13:12:35 -0700

    main.xaml

---
+++ WebsocketTool/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using WebsocketNetTool.Base;
+using WebsocketNetTool.WebSocket;
+using System.Windows;
+
+namespace WebsocketNetTool
+{
+    /// <summary>
+    /// MainWindow.xaml 
+    /// </summary>
+    public partial class MainWindow : BaseWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitEvents();
+        }
+
+    }
+}
