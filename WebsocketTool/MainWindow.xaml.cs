5149d35336f61f15efee5174fac9ab76b3e49997
atsus <atsushinee@outlook.com>
2022-07-17 13:13:17 -0700

    main.xaml func

--- WebsocketTool/MainWindow.xaml.cs
+++ WebsocketTool/MainWindow.xaml.cs
@@ -15,5 +15,21 @@ namespace WebsocketNetTool
             InitEvents();
         }
 
+
+     public override void InitEvents()
+        {
+            BtnWebsocketServer.Click += BtnWebsocketServer_Click;
+            BtnWebsocketClient.Click += BtnWebsocketClient_Click;
+        }
+
+        private void BtnWebsocketClient_Click(object sender, RoutedEventArgs e)
+        {
+            new WebsocketClientWindow().Show();
+        }
+
+        private void BtnWebsocketServer_Click(object sender, RoutedEventArgs e)
+        {
+            new WebsocketServerWindow().Show();
+        }
     }
 }
