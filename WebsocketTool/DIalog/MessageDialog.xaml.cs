d72938f0414d33471d8e1cd4b72446643bf600ac
atsus <atsushinee@outlook.com>
2022-07-25 21:23:47 -0700

    dialogs logic

--- WebsocketTool/DIalog/MessageDialog.xaml.cs
+++ WebsocketTool/DIalog/MessageDialog.xaml.cs
@@ -15,6 +15,32 @@ namespace WebsocketNetTool.Dialog
         }
 
 
-     
+        public static void Show(Window window, string message, Action<MessageDialog> action = null, bool showCancel = false, string btnName = "我知道了")
+        {
+            var dialog = new MessageDialog();
+            if (window == null)
+            {
+                dialog.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+            else
+            {
+                dialog.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                dialog.Owner = window;
+            }
+
+            dialog.TextMessage.Text = message;
+            dialog.BtnOK.Content = btnName;
+            dialog.BtnCancel.Visibility = showCancel ? Visibility.Visible : Visibility.Collapsed;
+            dialog.BtnCancel.Click += (sender, e) =>
+            {
+                dialog.Close();
+            };
+            dialog.BtnOK.Click += (sender, e) =>
+            {
+                dialog.Close();
+                action?.Invoke(dialog);
+            };
+            dialog.ShowDialog();
+        }
     }
 }
