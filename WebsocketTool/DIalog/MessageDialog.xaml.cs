2db0d3ea8b2c8d9964996bdae241d29c6c21f3be
atsus <atsushinee@outlook.com>
2022-07-25 21:23:31 -0700

    dialogs

---
+++ WebsocketTool/DIalog/MessageDialog.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace WebsocketNetTool.Dialog
+{
+    /// <summary>
+    /// MessageDialog.xaml
+    /// </summary>
+    public partial class MessageDialog : Window
+    {
+        private MessageDialog()
+        {
+            InitializeComponent();
+           
+        }
+
+
+     
+    }
+}
