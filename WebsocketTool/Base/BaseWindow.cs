e1edb3d0383071194a869d865b7e7d7626d9ba4d
atsus <atsushinee@outlook.com>
2022-07-26 21:22:49 -0700

    base

---
+++ WebsocketTool/Base/BaseWindow.cs
@@ -0,0 +1,18 @@
+﻿using WebsocketNetTool.Util;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace WebsocketNetTool.Base
+{
+    /// <summary>
+    /// BaseWindow.xaml
+    /// </summary>
+    public abstract partial class BaseWindow : Window
+    {
+    
+        public abstract void InitEvents();
+
+      
+    }
+}
