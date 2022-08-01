ab52a4ff9eff2a443a5019451938e565eb23cfd6
atsus <atsushinee@outlook.com>
2022-07-31 21:31:48 -0700

    websocket client

---
+++ WebsocketTool/Websocket/WebsocketClientWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WebsocketNetTool.Base;
+using WebsocketNetTool.Dialog;
+using WebsocketNetTool.Util;
+using SuperSocket.ClientEngine;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WebsocketNetTool.WebSocket
+{
+    /// <summary>
+    /// WebsocketServer.xaml 的交互逻辑
+    /// </summary>
+    public partial class WebsocketClientWindow : BaseWindow
+    {
+      
+    }
+}
