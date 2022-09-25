6c3e255c21a3d867cae50fcf0bd31618ebf7ed27
atsus <atsushinee@outlook.com>
2022-09-25 14:01:28 -0700

    update clean

--- websocket-client/lib/websocket/internal/test/xrand/xrand.go
+++ websocket-client/lib/websocket/internal/test/xrand/xrand.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-// Bytes generates random bytes with length n.
+
 func Bytes(n int) []byte {
 	b := make([]byte, n)
 	_, err := rand.Reader.Read(b)
@@ -17,7 +17,7 @@ func Bytes(n int) []byte {
 	return b
 }
 
-// String generates a random string with length n.
+
 func String(n int) string {
 	s := strings.ToValidUTF8(string(Bytes(n)), "_")
 	s = strings.ReplaceAll(s, "\x00", "_")
@@ -25,19 +25,19 @@ func String(n int) string {
 		return s[:n]
 	}
 	if len(s) < n {
-		// Pad with =
+		
 		extra := n - len(s)
 		return s + strings.Repeat("=", extra)
 	}
 	return s
 }
 
-// Bool returns a randomly generated boolean.
+
 func Bool() bool {
 	return Int(2) == 1
 }
 
-// Int returns a randomly generated integer between [0, max).
+
 func Int(max int) int {
 	x, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
 	if err != nil {
