2fb090b573ddb5cfd64234ebbca12a151d46f59e
李淳 <li.chun86@iwhalecloud.com>
2023-07-28 16:22:39 +0800

    update

--- websocket-client/sockjs-client/lib/transport-list.js
+++ websocket-client/sockjs-client/lib/transport-list.js
@@ -1,14 +1,12 @@
 'use strict';
 
 module.exports = [
-  // streaming transports
   require('./transport/websocket')
 , require('./transport/xhr-streaming')
 , require('./transport/xdr-streaming')
 , require('./transport/eventsource')
 , require('./transport/lib/iframe-wrap')(require('./transport/eventsource'))
 
-  // polling transports
 , require('./transport/htmlfile')
 , require('./transport/lib/iframe-wrap')(require('./transport/htmlfile'))
 , require('./transport/xhr-polling')
