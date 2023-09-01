ed45cf8cc7b9d467615f1b1ad6bb1d8ad16c0482
李淳 <li.chun86@iwhalecloud.com>
2023-09-01 16:34:14 +0800

    fix websocket chat

--- websocket-client/lib/websocket/examples/chat/index.js
+++ websocket-client/lib/websocket/examples/chat/index.js
@@ -1,7 +1,7 @@
 ;(() => {
-  // expectingMessage is set to true
-  // if the user has just submitted a message
-  // and so we should scroll the next message into view when received.
+  
+  
+  
   let expectingMessage = false
   function dial() {
     const conn = new WebSocket(`ws://${location.host}/subscribe`)
@@ -17,7 +17,7 @@
       console.info("websocket connected")
     })
 
-    // This is where we handle messages received.
+    
     conn.addEventListener("message", ev => {
       if (typeof ev.data !== "string") {
         console.error("unexpected message type", typeof ev.data)
@@ -36,10 +36,10 @@
   const publishForm = document.getElementById("publish-form")
   const messageInput = document.getElementById("message-input")
 
-  // appendLog appends the passed text to messageLog.
+  
   function appendLog(text, error) {
     const p = document.createElement("p")
-    // Adding a timestamp to each message makes the log easier to read.
+    
     p.innerText = `${new Date().toLocaleTimeString()}: ${text}`
     if (error) {
       p.style.color = "red"
@@ -50,7 +50,7 @@
   }
   appendLog("Submit a message to get started!")
 
-  // onsubmit publishes the message from the user when the form is submitted.
+  
   publishForm.onsubmit = async ev => {
     ev.preventDefault()
 
