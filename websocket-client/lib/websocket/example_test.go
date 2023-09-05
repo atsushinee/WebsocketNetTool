4f233c2f001097575ffff51d55c1c4938593025a
李淳 <li.chun86@iwhalecloud.com>
2023-09-05 19:55:22 +0800

    fix websocket chat

--- websocket-client/lib/websocket/example_test.go
+++ websocket-client/lib/websocket/example_test.go
@@ -11,8 +11,8 @@ import (
 )
 
 func ExampleAccept() {
-	// This handler accepts a WebSocket connection, reads a single JSON
-	// message from the client and then closes the connection.
+	
+	
 
 	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c, err := websocket.Accept(w, r, nil)
@@ -40,8 +40,8 @@ func ExampleAccept() {
 }
 
 func ExampleDial() {
-	// Dials a server, writes a single JSON message and then
-	// closes the connection.
+	
+	
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
@@ -61,8 +61,8 @@ func ExampleDial() {
 }
 
 func ExampleCloseStatus() {
-	// Dials a server and then expects to be disconnected with status code
-	// websocket.StatusNormalClosure.
+	
+	
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
@@ -80,8 +80,8 @@ func ExampleCloseStatus() {
 }
 
 func Example_writeOnly() {
-	// This handler demonstrates how to correctly handle a write only WebSocket connection.
-	// i.e you only expect to write messages and do not expect to read any messages.
+	
+	
 	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c, err := websocket.Accept(w, r, nil)
 		if err != nil {
@@ -118,8 +118,8 @@ func Example_writeOnly() {
 }
 
 func Example_crossOrigin() {
-	// This handler demonstrates how to safely accept cross origin WebSockets
-	// from the origin example.com.
+	
+	
 	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
 			OriginPatterns: []string{"example.com"},
@@ -135,64 +135,64 @@ func Example_crossOrigin() {
 	log.Fatal(err)
 }
 
-// This example demonstrates how to create a WebSocket server
-// that gracefully exits when sent a signal.
-//
-// It starts a WebSocket server that keeps every connection open
-// for 10 seconds.
-// If you CTRL+C while a connection is open, it will wait at most 30s
-// for all connections to terminate before shutting down.
-// func ExampleGrace() {
-// 	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		c, err := websocket.Accept(w, r, nil)
-// 		if err != nil {
-// 			log.Println(err)
-// 			return
-// 		}
-// 		defer c.Close(websocket.StatusInternalError, "the sky is falling")
-//
-// 		ctx := c.CloseRead(r.Context())
-// 		select {
-// 		case <-ctx.Done():
-// 		case <-time.After(time.Second * 10):
-// 		}
-//
-// 		c.Close(websocket.StatusNormalClosure, "")
-// 	})
-//
-// 	var g websocket.Grace
-// 	s := &http.Server{
-// 		Handler:      g.Handler(fn),
-// 		ReadTimeout:  time.Second * 15,
-// 		WriteTimeout: time.Second * 15,
-// 	}
-//
-// 	errc := make(chan error, 1)
-// 	go func() {
-// 		errc <- s.ListenAndServe()
-// 	}()
-//
-// 	sigs := make(chan os.Signal, 1)
-// 	signal.Notify(sigs, os.Interrupt)
-// 	select {
-// 	case err := <-errc:
-// 		log.Printf("failed to listen and serve: %v", err)
-// 	case sig := <-sigs:
-// 		log.Printf("terminating: %v", sig)
-// 	}
-//
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-// 	defer cancel()
-// 	s.Shutdown(ctx)
-// 	g.Shutdown(ctx)
-// }
 
-// This example demonstrates full stack chat with an automated test.
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+//
+
+
+//
+
+
+
+
+
+
+//
+
+
+
+
+//
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
 func Example_fullStackChat() {
-	// https://github.com/nhooyr/websocket/tree/master/examples/chat
+	
 }
 
-// This example demonstrates a echo server.
+
 func Example_echo() {
-	// https://github.com/nhooyr/websocket/tree/master/examples/echo
+	
 }
