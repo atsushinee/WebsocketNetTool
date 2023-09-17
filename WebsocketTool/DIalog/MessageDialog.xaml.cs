ae3db697fa073db0d72e1d4ba6838f9b9b614d47
atsus <atsushinee@outlook.com>
2023-09-17 20:18:28 +0800

    fix tool

--- WebsocketTool/DIalog/MessageDialog.xaml.cs
+++ WebsocketTool/DIalog/MessageDialog.xaml.cs
@@ -3,9 +3,9 @@ using System.Windows;
 
 namespace WebsocketNetTool.Dialog
 {
-    /// <summary>
-    /// MessageDialog.xaml
-    /// </summary>
+    
+    
+    
     public partial class MessageDialog : Window
     {
         private MessageDialog()
